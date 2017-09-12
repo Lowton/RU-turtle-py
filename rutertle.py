02a028151bf2232bfb9ee7712ceaa0e87d4057b2
Lowton <iskeyiv@gmail.com>
2017-09-12 10:14:54 +0400

    init

---
+++ rutertle.py
@@ -0,0 +1,29 @@
+from turtle import pen as перо
+from turtle import fd as вп # вперёд
+from turtle import bk as нз # назад
+from turtle import rt as пр # право
+from turtle import lt as лв # лево
+from turtle import goto as идти
+from turtle import setx as зг # задать горизонталь
+from turtle import sety as зв # задать вертикаль
+from turtle import seth as зу # задать угол
+
+from turtle import home as начало
+from turtle import circle as окружность
+from turtle import dot as точка
+
+
+from turtle import pos as позиция
+from turtle import heading as угол
+
+from turtle import down as оп # опустить перо
+from turtle import up as пп # поднять перо
+
+from turtle import pencolor as цвет
+
+from turtle import reset as сброс
+from turtle import clear as очистить
+
+
+def выход():
+    exit()
